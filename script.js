e50631f77524da561a57c66a609d8529d0e51273
Zaxutic <zax@zaxu.xyz>
2020-04-05 23:46:07 +1000

    Make everything better

--- script.js
+++ script.js
@@ -2,19 +2,8 @@ var span = document.getElementById('time');
 
 function time() {
   var d = new Date();
-  var s = d.getSeconds();
-  var m = d.getMinutes();
-  var h = d.getHours();
-  if (s < 10) {
-	s = "0" + s;
-  }
-  if (m < 10) {
-	m = "0" + m;
-  }
-  if (h < 10) {
-	h = "0" + h;
-  }
-  span.textContent = h + ":" + m + ":" + s;
+  span.textContent = d.toLocaleTimeString('en-GB');
 }
 
+time();
 setInterval(time, 1000);
